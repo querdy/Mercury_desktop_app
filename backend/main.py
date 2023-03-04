eb47d328c654fa4de876d9c3fda58fff38bce7fd
Boyara <querdy7@gmail.com>
2023-03-04 18:55:27 +0500

    fix db

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy_utils import database_exists, create_database
 
 from app.api_v1.router import api_router
 from app.core.config import settings
 from app.core.logger import init_logging
+from app.db.database import Base, engine
 
 app = FastAPI(
     title="vetis_parser",
@@ -24,6 +26,10 @@ app.add_middleware(
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
 init_logging()
+
+if not database_exists(engine.url):
+    create_database(engine.url)
+    Base.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
